e3e48f3b9a9d5be31f339ef99e49f6998928cade
Jorge <jorge.wellison@dce.ufpb.br>
2019-09-17 09:05:31 -0300

    create insert function

--- src/main/java/br/com/dcx/bookmark/resources/UsuarioResource.java
+++ src/main/java/br/com/dcx/bookmark/resources/UsuarioResource.java
@@ -1,12 +1,32 @@
 package br.com.dcx.bookmark.resources;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.dcx.bookmark.domain.Usuario;
+import br.com.dcx.bookmark.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
 
+
+	@Autowired
+	private UsuarioService service;
+
 	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Usuario usuario){
+		usuario = service.insert(usuario);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	
+}
 }
--- src/main/java/br/com/dcx/bookmark/services/UsuarioService.java
+++ src/main/java/br/com/dcx/bookmark/services/UsuarioService.java
@@ -1,9 +1,20 @@
 package br.com.dcx.bookmark.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.dcx.bookmark.domain.Usuario;
+import br.com.dcx.bookmark.repositories.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 
+	@Autowired
+	private UsuarioRepository userRepo;
 	
+	
+	public Usuario insert(Usuario usuario){
+		usuario.setId(null);
+		return userRepo.save(usuario);
+	}
 }
