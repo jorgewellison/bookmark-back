10f78c2007f67b9f07b08f6a32b86e1e61a6a5c0
mirianbatista <mirian.batista@dcx.ufpb.br>
2019-09-23 21:31:36 -0300

    classe SecurityConfig

---
+++ src/main/java/br/com/dcx/bookmark/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package br.com.dcx.bookmark.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+}
