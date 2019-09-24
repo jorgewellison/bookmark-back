3f2006813ecb80a1653b0e3e7a26cbbf4a0fd301
mirianbatista <mirian.batista@dcx.ufpb.br>
2019-09-23 21:33:49 -0300

    função public matchers

--- src/main/java/br/com/dcx/bookmark/config/SecurityConfig.java
+++ src/main/java/br/com/dcx/bookmark/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package br.com.dcx.bookmark.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+			"/livros/**"
+	};
 }
